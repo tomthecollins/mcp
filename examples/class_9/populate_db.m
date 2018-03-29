28d01c6d402e34702777f37e8738bc7f7ec58e61
Tom Collins <tomthecollins@dyn097009.cc.lehigh.edu>
2018-03-29 10:43:59 -0400

    Tweak to Shazam algorithm.

--- examples/class_9/populate_db.m
+++ examples/class_9/populate_db.m
@@ -7,28 +7,34 @@ addpath('./FastPeakFind');
 
 % Paths to audio files.
 audPaths = {...
-  ...%'./../class_7/Labyrinth.wav'
-  ...%'./../class_6/beat_tracking/Kendrick Lamar - HUMBLE.wav'
-  './fragment_up.wav'
-  './fragment_down.wav'
+  './audio/guitar.wav'...
+  './audio/piano.wav'...
+  './audio/strings.wav'...
+  % './audio/12 La Vie En Rose excerpt.mp3'...
+  % './audio/Jaymes Young - Don''t You Know.mp3'...
+  % './audio/02 The Greatest Man That Ever Lived (Variations On a Shaker Hymn).mp3'...
+  % './../class_7/Labyrinth.wav'
+  % './../class_6/beat_tracking/Kendrick Lamar - HUMBLE.wav'
   ...
   };
-naud = size(audPaths, 1);
+naud = size(audPaths, 2);
 
 % Spectrogram parameters.
-nfft = 2048;
+nfft = 8192;
 win = hann(nfft);
-overlap = nfft/4; % 25% overlap between adjacent spectra.
-nrows = 50; % Most of the spectrum (representing higher frequencies) is empty.
+overlap = 7*nfft/8; % 50% overlap between adjacent spectra.
+step = nfft - overlap;
+nrows = 500; % Most of the spectrum (representing higher frequencies) is empty.
 
 % Fingerprint parameters.
 timeThresMin = 0.1;
 timeThresMax = 1;
 pidxThresMin = 0;
-pidxThresMax = 5;
+pidxThresMax = 150;
 
 % Variable to hold all the fingerprints.
 dbase = struct;
+dbase.songs = [];
 cumuSamp = 0;
 for i=1:naud
   fprintf('Processing file %d of %d.\n', i, naud);
@@ -36,11 +42,13 @@ for i=1:naud
   [sig, Fs] = audioread(audPaths{i});
   % Spectrogram and peak pick.
   [s, w, t] = spectrogram(sig(:, 1), win, overlap, nfft); % Just left channel.
-  s = abs(s(1:50, :));
+  s = abs(s(1:nrows, :));
   % Show spectrogram.
-  % close all; imagesc(-s); colormap 'gray';
+  % close all; imagesc(-s); colormap 'gray'; axis xy
+  % xlabel('Time (Spectrogram Increment)', 'FontSize', 18);
+  % ylabel('Frequency (Spectrogram Increment)', 'FontSize', 18);
   % Pick peaks.
-  thres = quantile(s(:), 0.9);
+  thres = quantile(s(:), 0.975);
   IJ = FastPeakFind(s, thres);
   I = IJ(2:2:end);
   J = IJ(1:2:end);
@@ -55,24 +63,30 @@ for i=1:naud
     jj = ii + 1;
     while jj <= npeak
       ind2 = [I(jj) J(jj)];
-      time_diff = (ind2(2) - ind1(2))*overlap/Fs;
+      time_diff = (ind2(2) - ind1(2))*step/Fs;
       pitch_diff = abs(ind2(1) - ind1(1));
       % Decide whether to make a fingerprint.
       if time_diff > timeThresMin && time_diff < timeThresMax &&...
           pitch_diff > pidxThresMin && pitch_diff < pidxThresMax
         % Make a fingerprint. Start with the hash.
-        time_hash = num2str(time_diff);
-        time_hash = time_hash(3:4);
+        time_hash = num2str(round(1000*time_diff));
+        if time_diff < .01
+          time_hash = ['00' time_hash];
+        elseif time_diff < .1
+          time_hash = ['0' time_hash(1:2)];
+        else
+          time_hash = time_hash(1:3);
+        end
         pitch_hash = num2str(pitch_diff);
-        hash = ['fd' time_hash pitch_hash];
+        hash = ['fp' time_hash pitch_hash];
         if ~isfield(dbase, hash)
           % Make a new entry in dbase.
-          dbase.(hash) = struct('timestamp', cumuSamp + ind1(2)*overlap,...
+          dbase.(hash) = struct('timestamp', cumuSamp + ind1(2)*step,...
             'idpiece', audPaths{i});
         else
           % Add to an existing entry in dbase.
           dbase.(hash) = [dbase.(hash);...
-            struct('timestamp', cumuSamp + ind1(2)*overlap,...
+            struct('timestamp', cumuSamp + ind1(2)*step,...
             'idpiece', audPaths{i})];
         end
         nfp=nfp+1;
@@ -89,14 +103,16 @@ for i=1:naud
     end
   end
   % Plot some of the fingerprints.
-  hold on;
-  for ifp = 1:nfp
-    pause(0.05);
-    line([fp(ifp, 2) fp(ifp, 4)], [fp(ifp, 1) fp(ifp, 3)]);
-  end
-  hold off;
+  % hold on;
+  % for ifp = 1:nfp
+  %   pause(0.01);
+  %   line([fp(ifp, 2) fp(ifp, 4)], [fp(ifp, 1) fp(ifp, 3)]);
+  % end
+  % hold off;
   
   % Update cumuSamp.
+  dbase.songs = [dbase.songs...
+    struct('fnam', audPaths{i}, 'cumuSamp', cumuSamp)];
   cumuSamp = cumuSamp + size(sig, 1);
 end
 
--- examples/class_9/probe_db.m
+++ examples/class_9/probe_db.m
@@ -1,7 +1,7 @@
 % Copyright Tom Collins 3/25/2018
 
 % Probe db for a randomly selected and noisy audio clip.
-clipLength = 2.5;
+clipLength = 2;
 
 % Load dbase.
 load('dbase');
@@ -11,25 +11,30 @@ addpath('./FastPeakFind');
 
 % Paths to audio files.
 audPaths = {...
-  ...%'./../class_7/Labyrinth.wav'
-  ...%'./../class_6/beat_tracking/Kendrick Lamar - HUMBLE.wav'
-  './fragment_up.wav'
-  './fragment_down.wav'
+  './audio/guitar.wav'...
+  './audio/piano.wav'...
+  './audio/strings.wav'...
+  % './audio/12 La Vie En Rose excerpt.mp3'...
+  % './audio/Jaymes Young - Don''t You Know.mp3'...
+  % './audio/02 The Greatest Man That Ever Lived (Variations On a Shaker Hymn).mp3'...
+  % './../class_7/Labyrinth.wav'
+  % './../class_6/beat_tracking/Kendrick Lamar - HUMBLE.wav'
   ...
   };
-naud = size(audPaths, 1);
+naud = size(audPaths, 2);
 
 % Spectrogram parameters.
-nfft = 2048;
+nfft = 8192;
 win = hann(nfft);
-overlap = nfft/4; % 25% overlap between adjacent spectra.
-nrows = 50; % Most of the spectrum (representing higher frequencies) is empty.
+overlap = 7*nfft/8; % 25% overlap between adjacent spectra.
+step = nfft - overlap;
+nrows = 500; % Most of the spectrum (representing higher frequencies) is empty.
 
 % Fingerprint parameters.
 timeThresMin = 0.1;
 timeThresMax = 1;
 pidxThresMin = 0;
-pidxThresMax = 5;
+pidxThresMax = 150;
 
 % Choose an audio file at random.
 audPath = audPaths{randi(naud)};
@@ -41,19 +46,20 @@ nsamp = size(sig, 1);
 player = audioplayer(sig, Fs);
 play(player);
 % Add some noise.
-signoise = 0.92*sig + 0.08*2*(rand(nsamp, 1) - 0.5);
+% signoise = sig;
+signoise = 0.96*sig + 0.04*2*(rand(nsamp, 1) - 0.5);
 player2 = audioplayer(signoise, Fs);
 play(player2);
 
 % Spectrogram and peak pick.
 [s, w, t] = spectrogram(signoise, win, overlap, nfft);
-s = abs(s(1:50, :));
+s = abs(s(1:nrows, :));
 % Show spectrogram.
 close all; imagesc(-s); colormap 'gray'; axis xy;
-xlabel('Time (Spectrogram Increment)');
-ylabel('Frequency (Spectrogram Increment)')
+xlabel('Time (Spectrogram Increment)', 'FontSize', 18);
+ylabel('Frequency (Spectrogram Increment)', 'FontSize', 18);
 % Pick peaks.
-thres = quantile(s(:), 0.85);
+thres = quantile(s(:), 0.95);
 IJ = FastPeakFind(s, thres);
 I = IJ(2:2:end);
 J = IJ(1:2:end);
@@ -61,37 +67,51 @@ hold on; plot(J, I, 'r+'); hold off;
   
 % Create fingerprints.
 matches = [];
-npeak = size(I, 1);
 nmatch = 0;
+fp = [];
+nfp = 0;
+npeak = size(I, 1);
 for ii = 1:npeak
   ind1 = [I(ii) J(ii)];
   jj = ii + 1;
   while jj <= npeak
     ind2 = [I(jj) J(jj)];
-    time_diff = (ind2(2) - ind1(2))*overlap/Fs;
+    time_diff = (ind2(2) - ind1(2))*step/Fs;
     pitch_diff = abs(ind2(1) - ind1(1));
     % Decide whether to make a fingerprint.
     if time_diff > timeThresMin && time_diff < timeThresMax &&...
         pitch_diff > pidxThresMin && pitch_diff < pidxThresMax
       % Make a fingerprint. Start with the hash.
-      time_hash = num2str(time_diff);
-      time_hash = time_hash(3:4);
+      time_hash = num2str(round(1000*time_diff));
+      if time_diff < .01
+        time_hash = ['00' time_hash];
+      elseif time_diff < .1
+        time_hash = ['0' time_hash(1:2)];
+      else
+        time_hash = time_hash(1:3);
+      end
       pitch_hash = num2str(pitch_diff);
-      hash = ['fd' time_hash pitch_hash];
+      hash = ['fp' time_hash pitch_hash];
       if ~isfield(dbase, hash)
         % No fp found.
       else
         % Fp found!
+        % fprintf('ii = %d, jj = %d.\n', ii, jj);
         tstampIdPairs = dbase.(hash);
         npair = size(tstampIdPairs, 1);
         for ipair = 1:npair
           currPair = tstampIdPairs(ipair);
           matches = [matches;...
-            struct('dbts', currPair.timestamp, 'pbts', ind1(2)*overlap,...
+            struct('dbts', currPair.timestamp, 'pbts', ind1(2)*step,...
             'idpiece', currPair.idpiece)];
           nmatch=nmatch+1;
         end
       end
+      nfp=nfp+1;
+      fp = [fp; [ind1 ind2]];
+      % fprintf(...
+      %   'Made a fp for ind1 = [%d, %d] and ind2 = [%d, %d].\n',...
+      %   ind1(1), ind1(2), ind2(1), ind2(2));
     end
     % Move on to the next value of ii if we are too far away in time.
     if time_diff >= timeThresMax
@@ -100,19 +120,66 @@ for ii = 1:npeak
     jj=jj+1;
   end
 end
+% Plot some of the fingerprints.
+hold on;
+for ifp = 1:nfp
+  pause(0.01);
+  line([fp(ifp, 2) fp(ifp, 4)], [fp(ifp, 1) fp(ifp, 3)]);
+end
+hold off;
 
 % Plot of matches.
-plot([matches.dbts], [matches.pbts], '.b');
+figure; plot([matches.dbts], [matches.pbts], '.b');
+xlabel('Time in Database (Samples)', 'FontSize', 18);
+ylabel('Time in Query (Samples)', 'FontSize', 18);
+for i = 1:naud
+  line([dbase.songs(i).cumuSamp + 1 ...
+        dbase.songs(i).cumuSamp + 1],...
+       [0 max([matches.pbts] + 500)],...
+       'Color', 'k', 'LineStyle', '--');
+end
+% Histogram.
 figure;
+hist([matches.dbts] - [matches.pbts], 100)
+xlabel('Transformed Time in Database (Samples)', 'FontSize', 18);
+ylabel('Number of Matches', 'FontSize', 18);
 [counts, centers] = hist([matches.dbts] - [matches.pbts], 100);
+[C, I] = max(counts);
+
+% Determine name of winning song from max in histogram.
+winningSample = centers(I);
+winningSongIdx = find(winningSample >= [dbase.songs.cumuSamp], 1, 'last');
+% THIS IS THE SONG 'SHAZAM' THINKS IS IN THE CLIP!
+winningSongNam = dbase.songs(winningSongIdx).fnam
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 % Example transformation.
 x = [4 5 6 7 8 9];
 y = [1 2 3 4 5 6];
-z = x - y;
-plot(z, y, '.b');
+close all
+plot(x, y, '.b');
 xlim([0 10])
 ylim([0 7])
-
+xlabel('Time in Database (Samples)', 'FontSize', 18);
+ylabel('Time in Query (Samples)', 'FontSize', 18);
+% Apply transformation.
+z = x - y;
+figure; plot(z, y, '.b');
+xlim([0 10])
+ylim([0 7])
+xlabel('Transformed Time in Database (Samples)', 'FontSize', 18);
+ylabel('Time in Query (Samples)', 'FontSize', 18);
